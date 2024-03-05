577f005ea5cbe7cb9f1e085da6f2b9c4ddda3f51
Dan <36249892+dante-militello@users.noreply.github.com>
2024-03-05 04:27:12 -0300

    removed instance_menu

--- packages/frontend/src/ui/_common_/common.ts
+++ packages/frontend/src/ui/_common_/common.ts
@@ -41,81 +41,81 @@ function toolsMenuItems(): MenuItem[] {
 }
 
 export function openInstanceMenu(ev: MouseEvent) {
-	os.popupMenu([{
-		text: instance.name ?? host,
-		type: 'label',
-	}, {
-		type: 'link',
-		text: i18n.ts.instanceInfo,
-		icon: 'ti ti-info-circle',
-		to: '/about',
-	}, {
-		type: 'link',
-		text: i18n.ts.customEmojis,
-		icon: 'ti ti-icons',
-		to: '/about#emojis',
-	}, {
-		type: 'link',
-		text: i18n.ts.federation,
-		icon: 'ti ti-whirl',
-		to: '/about#federation',
-	}, {
-		type: 'link',
-		text: i18n.ts.charts,
-		icon: 'ti ti-chart-line',
-		to: '/about#charts',
-	}, { type: 'divider' }, {
-		type: 'link',
-		text: i18n.ts.ads,
-		icon: 'ti ti-ad',
-		to: '/ads',
-	}, ($i && ($i.isAdmin || $i.policies.canInvite) && instance.disableRegistration) ? {
-		type: 'link',
-		to: '/invite',
-		text: i18n.ts.invite,
-		icon: 'ti ti-user-plus',
-	} : undefined, {
-		type: 'parent',
-		text: i18n.ts.tools,
-		icon: 'ti ti-tool',
-		children: toolsMenuItems(),
-	}, { type: 'divider' }, (instance.impressumUrl) ? {
-		text: i18n.ts.impressum,
-		icon: 'ti ti-file-invoice',
-		action: () => {
-			window.open(instance.impressumUrl, '_blank', 'noopener');
-		},
-	} : undefined, (instance.tosUrl) ? {
-		text: i18n.ts.termsOfService,
-		icon: 'ti ti-notebook',
-		action: () => {
-			window.open(instance.tosUrl, '_blank', 'noopener');
-		},
-	} : undefined, (instance.privacyPolicyUrl) ? {
-		text: i18n.ts.privacyPolicy,
-		icon: 'ti ti-shield-lock',
-		action: () => {
-			window.open(instance.privacyPolicyUrl, '_blank', 'noopener');
-		},
-	} : undefined, (!instance.impressumUrl && !instance.tosUrl && !instance.privacyPolicyUrl) ? undefined : { type: 'divider' }, {
-		text: i18n.ts.help,
-		icon: 'ti ti-help-circle',
-		action: () => {
-			window.open('https://misskey-hub.net/docs/for-users/', '_blank', 'noopener');
-		},
-	}, ($i) ? {
-		text: i18n.ts._initialTutorial.launchTutorial,
-		icon: 'ti ti-presentation',
-		action: () => {
-			os.popup(defineAsyncComponent(() => import('@/components/MkTutorialDialog.vue')), {}, {}, 'closed');
-		},
-	} : undefined, {
-		type: 'link',
-		text: i18n.ts.aboutMisskey,
-		to: '/about-misskey',
-	}], ev.currentTarget ?? ev.target, {
-		align: 'left',
-	});
+	// os.popupMenu([{
+	// 	text: instance.name ?? host,
+	// 	type: 'label',
+	// }, {
+	// 	type: 'link',
+	// 	text: i18n.ts.instanceInfo,
+	// 	icon: 'ti ti-info-circle',
+	// 	to: '/about',
+	// }, {
+	// 	type: 'link',
+	// 	text: i18n.ts.customEmojis,
+	// 	icon: 'ti ti-icons',
+	// 	to: '/about#emojis',
+	// }, {
+	// 	type: 'link',
+	// 	text: i18n.ts.federation,
+	// 	icon: 'ti ti-whirl',
+	// 	to: '/about#federation',
+	// }, {
+	// 	type: 'link',
+	// 	text: i18n.ts.charts,
+	// 	icon: 'ti ti-chart-line',
+	// 	to: '/about#charts',
+	// }, { type: 'divider' }, {
+	// 	type: 'link',
+	// 	text: i18n.ts.ads,
+	// 	icon: 'ti ti-ad',
+	// 	to: '/ads',
+	// }, ($i && ($i.isAdmin || $i.policies.canInvite) && instance.disableRegistration) ? {
+	// 	type: 'link',
+	// 	to: '/invite',
+	// 	text: i18n.ts.invite,
+	// 	icon: 'ti ti-user-plus',
+	// } : undefined, {
+	// 	type: 'parent',
+	// 	text: i18n.ts.tools,
+	// 	icon: 'ti ti-tool',
+	// 	children: toolsMenuItems(),
+	// }, { type: 'divider' }, (instance.impressumUrl) ? {
+	// 	text: i18n.ts.impressum,
+	// 	icon: 'ti ti-file-invoice',
+	// 	action: () => {
+	// 		window.open(instance.impressumUrl, '_blank', 'noopener');
+	// 	},
+	// } : undefined, (instance.tosUrl) ? {
+	// 	text: i18n.ts.termsOfService,
+	// 	icon: 'ti ti-notebook',
+	// 	action: () => {
+	// 		window.open(instance.tosUrl, '_blank', 'noopener');
+	// 	},
+	// } : undefined, (instance.privacyPolicyUrl) ? {
+	// 	text: i18n.ts.privacyPolicy,
+	// 	icon: 'ti ti-shield-lock',
+	// 	action: () => {
+	// 		window.open(instance.privacyPolicyUrl, '_blank', 'noopener');
+	// 	},
+	// } : undefined, (!instance.impressumUrl && !instance.tosUrl && !instance.privacyPolicyUrl) ? undefined : { type: 'divider' }, {
+	// 	text: i18n.ts.help,
+	// 	icon: 'ti ti-help-circle',
+	// 	action: () => {
+	// 		window.open('https://misskey-hub.net/docs/for-users/', '_blank', 'noopener');
+	// 	},
+	// }, ($i) ? {
+	// 	text: i18n.ts._initialTutorial.launchTutorial,
+	// 	icon: 'ti ti-presentation',
+	// 	action: () => {
+	// 		os.popup(defineAsyncComponent(() => import('@/components/MkTutorialDialog.vue')), {}, {}, 'closed');
+	// 	},
+	// } : undefined, {
+	// 	type: 'link',
+	// 	text: i18n.ts.aboutMisskey,
+	// 	to: '/about-misskey',
+	// }], ev.currentTarget ?? ev.target, {
+	// 	align: 'left',
+	// });
 }
 
 export function openToolsMenu(ev: MouseEvent) {
